fb299e7de41c2a3807b497364662d743adcb9844
ozankaze <ozankaze101@gmail.com>
2018-08-01 14:34:17 +0700

    bagian statis

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+    <footer>
+        1 Agustus 2018
+    </footer>
+    </body>
+</html>
---
+++ templates/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="assets/style.css">
+    <title>Login Dan Register OOP</title>
+</head>
+    <body>
+        <header>
+            <h3>Belajar bareng Bang Fauzan</h3>
+            <nav>
+                <a href="login.php">Login</a>
+                <a href="register.php">Register</a>
+                <a href="profile.php">Profile</a>
+            </nav>
+        </header>
